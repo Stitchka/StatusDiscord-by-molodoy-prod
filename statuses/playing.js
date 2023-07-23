e2786ac28a9484d2e7ef41ac582a81dcaedde100
molodoy.paren <139953049+Stitchka@users.noreply.github.com>
2023-07-24 02:43:27 +0800

    playing.js

--- statuses/playing.js
+++ statuses/playing.js
@@ -1,5 +1,5 @@
 
 // Change this to the name of the game you want
-const GAME_NAME = "Cyberpunk 2077";
+const GAME_NAME = "Discord";
 
 module.exports = (client) => client.user.setActivity(GAME_NAME, { type: "PLAYING" });
