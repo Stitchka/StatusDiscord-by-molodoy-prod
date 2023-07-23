bb4d8258f1d83546c4fad5bd51558caa3de1f40a
molodoy.paren <139953049+Stitchka@users.noreply.github.com>
2023-07-24 02:44:29 +0800

    listening.js

--- statuses/listening.js
+++ statuses/listening.js
@@ -3,9 +3,9 @@ const
   rpcGenerator = require("discordrpcgenerator"),
 
   // Add your client id in the index.js file
-  IMAGE = "The name of the image",
-  SONG = "The name of the song",
-  ARTIST = "The artist of the song";
+  IMAGE = "vk",
+  SONG = "ᴍᴏʟᴏᴅᴏʏ'ᴘʀᴏᴅ",
+  ARTIST = "ᴍᴏʟᴏᴅᴏʏ'ᴘʀᴏᴅ";
 
 module.exports = (client, CLIENT_ID) => rpcGenerator.getRpcImage(CLIENT_ID, IMAGE)
   .then(image => client.user.setPresence(
